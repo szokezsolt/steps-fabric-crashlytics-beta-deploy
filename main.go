3ef8647271a3aeb45bd4ecef3023a69ffb87c8a2
Szoke Zsolt <szokezsolt88@gmail.com>
2017-06-19 16:58:19 +0200

    In progress. DSYM uploading not finished, inputs hard-wired: 	apiKey := "" 	buildSecret := "" 	emailList := "" 	groupAliasesList := "" 	notification := "" 	releaseNotes := ""

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/exec"
 	"path/filepath"
 
 	"github.com/bitrise-io/go-utils/command"
@@ -23,12 +22,8 @@ func main() {
 
 	ipaPath := os.Getenv("ipa_path")
 	dsymPath := os.Getenv("dsym_path")
-
-	currentDir, err := pathutil.CurrentWorkingDirectoryAbsolutePath()
-	if err != nil {
-		log.Errorft("Failed to retrieve current working directory, error: %s", err)
-		os.Exit(1)
-	}
+	frameworkPath := os.Getenv("framework_path")
+	workDir := os.Getenv("work_dir")
 
 	// Validate parameters
 	log.Infoft("Configs:")
@@ -77,68 +72,108 @@ func main() {
 		configReleaseNotesPth = filepath.Join(configReleaseNotesPth, "app_releaseNotes.txt")
 		fileutil.WriteStringToFile(configReleaseNotesPth, releaseNotes)
 
-		// - Optional params
-		paramEmails := ""
-		if emailList != "" {
-			paramEmails = fmt.Sprintf("-emails " + emailList)
-		}
-
-		paramGroups := ""
-		if groupAliasesList != "" {
-			paramGroups = fmt.Sprintf("-groupAliases " + groupAliasesList)
-		}
-
-		configIsSendNotifications := "YES"
-		if notification == "No" {
-			configIsSendNotifications = "NO"
-		}
-
 		// - Submit IPA
 		log.Infoft("Submitting IPA...")
-		submitIPACmd := filepath.Join(currentDir, "Fabric/submit")
-		submitIPACmd += fmt.Sprintf("\\%s \\%s", apiKey, buildSecret)
-		submitIPACmd += fmt.Sprintf("-ipaPath \\%s -notesPath \\%s", ipaPath, configReleaseNotesPth)
-		submitIPACmd += fmt.Sprintf("-notifications \\%s%s%s", configIsSendNotifications, paramEmails, paramGroups)
-		log.Printf(submitIPACmd)
+		submitIPAPth := ""
+		if pathutil.IsRelativePath(frameworkPath) {
+			submitIPAPth = filepath.Join(workDir, frameworkPath, "submit")
+		} else {
+			submitIPAPth = filepath.Join(frameworkPath, "submit")
+		}
+
+		if pathExist, _ := pathutil.IsPathExists(submitIPAPth); pathExist == false {
+			submitIPAPth = filepath.Join(workDir, "Fabric/submit")
+		}
+
+		submitIPACmd := []string{}
+		submitIPACmd = append(submitIPACmd, submitIPAPth)
+		submitIPACmd = append(submitIPACmd, apiKey)
+		submitIPACmd = append(submitIPACmd, buildSecret)
+		submitIPACmd = append(submitIPACmd, "-ipaPath")
+		submitIPACmd = append(submitIPACmd, ipaPath)
+		submitIPACmd = append(submitIPACmd, "-notesPath")
+		submitIPACmd = append(submitIPACmd, configReleaseNotesPth)
+
+		// - Optional params
+		if emailList != "" {
+			submitIPACmd = append(submitIPACmd, "-emails")
+			submitIPACmd = append(submitIPACmd, emailList)
+		}
+
+		if groupAliasesList != "" {
+			submitIPACmd = append(submitIPACmd, "-groupAliases")
+			submitIPACmd = append(submitIPACmd, groupAliasesList)
+		}
+
+		submitIPACmd = append(submitIPACmd, "-notifications")
+		if notification == "No" {
+			submitIPACmd = append(submitIPACmd, "NO")
+		} else {
+			submitIPACmd = append(submitIPACmd, "YES")
+		}
+
+		log.Printf("%v", submitIPACmd)
 		log.Printf("\n")
 
-		//out, err := exec.Command(submitCmd).Output()
-		out, err := command.RunCmdAndReturnExitCode(exec.Command(submitIPACmd))
+		cmd, err := command.NewFromSlice(submitIPACmd)
 		if err != nil {
-			log.Errorft(fmt.Sprintf("Error submitting the IPA, error: %s", err))
-			os.Exit(1)
-		} else if out == 0 {
-			log.Doneft("IPA successfully submitted")
-		} else {
-			log.Errorft("IPA submit failed")
+			log.Errorft(fmt.Sprintf("Error creating IPA command from slice, error: %s", err))
 			os.Exit(1)
 		}
+
+		out, err := cmd.RunAndReturnTrimmedCombinedOutput()
+		if err != nil {
+			log.Errorft(fmt.Sprintf("Error submitting the IPA, output:\n%s, \nerror: %s", out, err))
+			os.Exit(1)
+		}
+		log.Printf(out)
+		log.Printf("\n")
 	}
 
 	// - Submit DSYM
 	if err := input.ValidateIfNotEmpty(dsymPath); err == nil {
 		isPathExists, err := pathutil.IsPathExists(dsymPath)
 		if isPathExists == false {
-			log.Errorft("DSYM path defined, but the file does not exist at path: %s", ipaPath)
+			log.Errorft("DSYM path defined, but the file does not exist at path: %s", dsymPath)
 		} else if err != nil {
 			log.Errorft("Failed to retrieve the contents of DSYM path, error: %s", err)
 		}
 
 		log.Infoft("Submitting DSYM...")
-		submitDSYMCmd := filepath.Join(currentDir, "Fabric/upload-symbols")
-		submitDSYMCmd += fmt.Sprintf("-a \\%s -p ios \\%s", apiKey, dsymPath)
-		log.Printf(submitDSYMCmd)
+		submitDSYMPth := ""
+		if pathutil.IsRelativePath(frameworkPath) {
+			submitDSYMPth = filepath.Join(workDir, frameworkPath, "uploadDSYM")
+		} else {
+			submitDSYMPth = filepath.Join(frameworkPath, "uploadDSYM")
+		}
+
+		if pathExist, _ := pathutil.IsPathExists(submitDSYMPth); pathExist == false {
+			submitDSYMPth = filepath.Join(workDir, "Fabric/uploadDSYM")
+		}
+
+		submitDSYMCmd := []string{}
+		submitDSYMCmd = append(submitDSYMCmd, submitDSYMPth)
+		submitDSYMCmd = append(submitDSYMCmd, "-a")
+		submitDSYMCmd = append(submitDSYMCmd, apiKey)
+		submitDSYMCmd = append(submitDSYMCmd, "-p")
+		submitDSYMCmd = append(submitDSYMCmd, "ios")
+		submitDSYMCmd = append(submitDSYMCmd, dsymPath)
+
+		log.Printf("%v", submitDSYMCmd)
 		log.Printf("\n")
 
-		out, err := command.RunCmdAndReturnExitCode(exec.Command(submitDSYMCmd))
+		cmd, err := command.NewFromSlice(submitDSYMCmd)
 		if err != nil {
-			log.Errorft(fmt.Sprintf("Error submitting the DSYM, error: %s", err))
-			os.Exit(1)
-		} else if out == 0 {
-			log.Doneft("DSYM successfully submitted")
-		} else {
-			log.Errorft("DSYM submit failed")
+			log.Errorft(fmt.Sprintf("Error creating DSYM command from slice, error: %s", err))
 			os.Exit(1)
 		}
+
+		out, err := cmd.RunAndReturnTrimmedCombinedOutput()
+		if err != nil {
+			log.Errorft(fmt.Sprintf("Error submitting the DSYM, output:\n%s, \nerror: %s", out, err))
+			os.Exit(1)
+		}
+
+		log.Printf(out)
 	}
 }
